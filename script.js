e7f451842ade3459893b0ccac752120667b085b6
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-01-28 11:40:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let outScreen = document.querySelector('.output input');
+mode = 'calc';
+
+function insert(num) {
+  outScreen.value += num;
+  if (mode == 'equal') {
+    outScreen.value = '';
+    outScreen.value += num;
+    mode = 'calc';
+  };
+};
+
+function clr() {
+  outScreen.value = '';
+};
+
+function del() {
+  outScreen.value = outScreen.value.slice(0, -1);
+  if (mode == 'equal') {
+    outScreen.value = '';
+  };
+};
+
+function calc() {
+  try {
+    outScreen.value = eval(outScreen.value);
+    mode = 'equal';
+  } catch (err) {
+    outScreen.value = 'INVALID';
+    mode = 'equal';
+  };
+};
